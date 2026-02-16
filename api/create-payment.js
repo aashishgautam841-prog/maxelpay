2749bee3f02c599ae4746c0c92d46c23ebc9b17b
aashishgautam841-prog <aashishgautam841@gmail.com>
2026-02-16 16:36:12 +0530

    add backend
    Removed unnecessary export statement and semicolon.

--- api/create-payment.js
+++ api/create-payment.js
@@ -4,8 +4,6 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-export default async function handler(req, res) {
-;
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
