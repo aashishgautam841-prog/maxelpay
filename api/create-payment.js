6e7ad533a2808ed3c2bce43167632a97a9e24028
aashishgautam841-prog <aashishgautam841@gmail.com>
2026-02-16 14:12:40 +0530

    add backend

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,11 +1,3 @@
-export default async function handler(req, res) {
-
-  if (req.method !== "POST") {
-    res.status(405).send("Only POST allowed");
-    return;
-  }
-
-  res.status(200).json({
-    message: "Backend is working"
-  });
+export default function handler(req, res) {
+  res.status(200).json({ message: "Backend working" });
 }
