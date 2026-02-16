e7057f4a0ac9e12135a3d0f222791501631d43ec
aashishgautam841-prog <aashishgautam841@gmail.com>
2026-02-16 16:22:32 +0530

    add backend

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,8 +1,10 @@
 export default async function handler(req, res) {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
-
   try {
     const { amount, orderId } = req.body;
 
