369adf6bb75ec3418c62ec230f7227d08fef89c1
aashishgautam841-prog <aashishgautam841@gmail.com>
2026-02-18 13:42:39 +0530

    add backend

--- api/create-payment.js
+++ api/create-payment.js
@@ -31,6 +31,7 @@ export default async function handler(req, res) {
         body: JSON.stringify({
           orderID: orderId,
           amount: amount,
+          userEmail: "aashishgautam841@gmail.com",
           currency: "USD",
           timestamp: timestamp,
           redirectUrl: "https://greenleaf.website/payment-success",
